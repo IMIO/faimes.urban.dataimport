b74e6a4cef9b842c2a2ac82efff8c6ee753c1f33
jjaumotte <julien.jaumotte@imio.be>
2017-01-19 16:13:30 +0100

    Faimes custom urbaweb process Fix buildout cfg

--- src/faimes/urban/dataimport/licences/importer.py
+++ src/faimes/urban/dataimport/licences/importer.py
@@ -2,6 +2,9 @@
 
 from zope.interface import implements
 
+from faimes.urban.dataimport.licences import objectsmapping
+from faimes.urban.dataimport.licences import valuesmapping
+from imio.urban.dataimport.mapping import ObjectsMapping, ValuesMapping
 from imio.urban.dataimport.urbaweb.importer import UrbawebDataImporter
 from faimes.urban.dataimport.interfaces import IFaimesDataImporter
 
@@ -10,3 +13,21 @@ class FaimesDataImporter(UrbawebDataImporter):
     """ """
 
     implements(IFaimesDataImporter)
+
+
+class LicencesMapping(ObjectsMapping):
+    """ """
+
+    def getObjectsNesting(self):
+        return objectsmapping.OBJECTS_NESTING
+
+    def getFieldsMapping(self):
+        return objectsmapping.FIELDS_MAPPINGS
+
+
+class LicencesValuesMapping(ValuesMapping):
+    """ """
+
+    def getValueMapping(self, mapping_name):
+
+        return valuesmapping.VALUES_MAPS.get(mapping_name, None)
